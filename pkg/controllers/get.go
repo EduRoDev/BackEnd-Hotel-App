9d2d0b8001d2c6b43afa4c6879a6d11b95d9657d
DxKaizer <solanoe934@gmail.com>
2024-08-22 15:27:35 -0500

    Creacion de la base de datos y los Protocolos GET,POST

--- pkg/controllers/get.go
+++ pkg/controllers/get.go
@@ -1,27 +1,26 @@
 package controllers
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+
+	"github.com/DxKaizer/Hotel_Backend/pkg/database"
+	"github.com/DxKaizer/Hotel_Backend/pkg/model"
 )
 
 type Hotel struct {
 	l *log.Logger
 }
 
-// ServeHTTP implements http.Handler.
-func (h *Hotel) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	Get(w, r)
-}
-
 func NewLogger(l *log.Logger) *Hotel {
 	return &Hotel{l}
 }
 
-func Get(w http.ResponseWriter, r *http.Request) {
+func (h *Hotel) GetHotel(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "application/json")
-	rp := map[string]string{
-		"message": "Hola mundo, como estas ?",
-	}
-	w.Write([]byte(rp["message"]))
+	data := model.Hotels{}
+	database.Database.Find(&data)
+	w.WriteHeader(http.StatusAccepted)
+	json.NewEncoder(w).Encode(data)
 }
--- pkg/model/models.go
+++ pkg/model/models.go
@@ -1,25 +1,54 @@
 package model
 
+import (
+	"fmt"
+
+	"github.com/DxKaizer/Hotel_Backend/pkg/database"
+)
+
 type Hotel struct {
 	Id          int    `json:"id"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Stars       int    `json:"stars"`
+	Name        string `gorm:"type:varchar(100)" json:"name"`
+	Description string `gorm:"type:varchar(100)" json:"description"`
 	Rooms       int    `json:"rooms"`
 }
 
+type Hotels []Hotel
+
 type Room struct {
 	Id          int    `json:"id"`
-	HotelId     int    `json:"hotel_id"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
+	Name        string `gorm:"type:varchar(100)" json:"name"`
+	Description string `gorm:"type:varchar(100)" json:"description"`
 	Price       int    `json:"price"`
+	HotelId     int    `json:"hotel_id"`
+	Hotel       Hotel  `gorm:"foreignKey:HotelId" json:"hotel"`
 }
 
-type Customers struct {
+type Rooms []Room
+
+type Customer struct {
 	Id       int    `json:"id"`
-	Name     string `json:"name"`
-	Email    string `json:"email"`
+	Name     string `gorm:"type:varchar(100)" json:"name"`
+	Email    string `gorm:"type:varchar(100)" json:"email"`
 	Phone    string `json:"phone"`
-	Password string `json:"password"`
+	Password string `gorm:"type:varchar(100)" json:"password"`
+	RoomID   int    `json:"room_id"`
+	Room     Room   `gorm:"foreignKey:RoomID" json:"room"`
+}
+
+type Customers []Customer
+
+func Migraciones() {
+	if database.Database == nil {
+		fmt.Print("Database connection is not established.\n")
+		return
+	}
+
+	err := database.Database.AutoMigrate(&Hotel{}, &Room{}, &Customer{})
+	if err != nil {
+		fmt.Print("Error during migration:", err)
+		return
+	}
+
+	fmt.Print("Database migration completed successfully.\n")
 }
